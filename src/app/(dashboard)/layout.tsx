658c4c471f0c6b4002a60f6c4ce760efbfe3fddb
RaviRafaliya11 <ravirafaliya11@gmail.com>
2025-06-24 09:10:04 -0400

    09: agents setup trpc

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { DashboardNavbar } from "@/modules/auth/ui/views/dashboard/ui/components/dashboard-navbar";
-import { DashboardSidebar } from "@/modules/auth/ui/views/dashboard/ui/components/dashboard-sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 
 interface Props {
   children: React.ReactNode;
